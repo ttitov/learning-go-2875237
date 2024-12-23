cd7ef10f55613589e9b576067ce26b919e140389
Timur Titov <t.titov@criteo.com>
2024-12-23 11:55:47 +0200

    str conversion

--- practice/main.go
+++ practice/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 const aConst int = 64
@@ -13,4 +15,14 @@ func main() {
 	fmt.Print("Enter some text: ")
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You enter: ", input)
+
+	fmt.Print("Enter a number: ")
+	inputNumber, _ := reader.ReadString('\n')
+	aFloat, err := strconv.ParseFloat(strings.TrimSpace(inputNumber), 64)
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println("Value of the number: ", aFloat)
+	}
+
 }
