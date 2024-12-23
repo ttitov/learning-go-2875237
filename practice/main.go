367fcb126e34878df766ed37129d550c4dde76ed
Timur Titov <t.titov@criteo.com>
2024-12-23 23:10:54 +0200

    float numbers

--- practice/main.go
+++ practice/main.go
@@ -10,4 +10,9 @@ func main() {
 	intSum := i1 + i2 + i3
 	fmt.Println("Integers sum is ", intSum)
 
+	f1, f2, f3 := 23.5, 55.1, 6.3
+
+	floatSum := f1 + f2 + f3
+	fmt.Println("Float sum is ", floatSum)
+
 }
