349f6b6dd7472e09ef38d3e5c97f3a633a6fff49
Timur Titov <t.titov@criteo.com>
2024-12-23 14:01:54 +0200

    some math

--- practice/main.go
+++ practice/main.go
@@ -1,28 +1,13 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
 )
 
-const aConst int = 64
-
 func main() {
-	reader := bufio.NewReader(os.Stdin)
-	fmt.Print("Enter some text: ")
-	input, _ := reader.ReadString('\n')
-	fmt.Println("You enter: ", input)
+	i1, i2, i3 := 1, 3, 45
 
-	fmt.Print("Enter a number: ")
-	inputNumber, _ := reader.ReadString('\n')
-	aFloat, err := strconv.ParseFloat(strings.TrimSpace(inputNumber), 64)
-	if err != nil {
-		fmt.Println(err)
-	} else {
-		fmt.Println("Value of the number: ", aFloat)
-	}
+	intSum := i1 + i2 + i3
+	fmt.Println("Integers sum is ", intSum)
 
 }
