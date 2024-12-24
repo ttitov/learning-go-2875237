d7d8063ac6b2fee7a608d18d2e11ae88a5a9e298
Timur Titov <timur.titov@gmail.com>
2024-12-24 22:48:33 +0200

    added and delet values in map

--- maps/main.go
+++ maps/main.go
@@ -8,6 +8,16 @@ func main() {
 	states := make(map[string]string)
 	fmt.Println(states)
 	states["WA"] = "Washington"
+	states["OR"] = "Oregon"
+	states["CA"] = "California"
+
+	fmt.Println(states)
+	california := states["CA"]
+	fmt.Println(california)
+
+	delete(states, "OR")
+	fmt.Println(states)
+	states["NY"] = "New York"
 	fmt.Println(states)
 
 }
