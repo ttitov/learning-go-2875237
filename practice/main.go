2d8faa92811c8c243db5f03d86f1b874cfda47f7
Timur Titov <t.titov@criteo.com>
2024-12-24 11:24:03 +0200

    date and time demo with Format

--- practice/main.go
+++ practice/main.go
@@ -2,9 +2,17 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Date and Time")
+	n := time.Now()
+	fmt.Println("Date and Time now: ", n)
+
+	t := time.Date(2009, time.November, 10, 23, 0, 0, 0, time.Local)
+
+	fmt.Println("Go launched at: ", t)
+
+	fmt.Println(t.Format(time.ANSIC))
 
 }
