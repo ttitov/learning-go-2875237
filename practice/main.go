26ead3dda249aa938aa117bd8bc422357e44e89f
Timur Titov <t.titov@criteo.com>
2024-12-24 10:46:31 +0200

    date and time demo

--- practice/main.go
+++ practice/main.go
@@ -2,25 +2,9 @@ package main
 
 import (
 	"fmt"
-	"math"
 )
 
 func main() {
-	i1, i2, i3 := 1, 3, 45
-
-	intSum := i1 + i2 + i3
-	fmt.Println("Integers sum is ", intSum)
-
-	f1, f2, f3 := 23.5, 55.1, 6.3
-
-	floatSum := f1 + f2 + f3
-	fmt.Println("Float sum is ", floatSum)
-
-	floatSum = math.Round(floatSum*100) / 100 //fix precision
-	fmt.Println("This is new sum: ", floatSum)
-
-	circleRadius := 15.5
-	circumference := circleRadius * 2 * math.Pi
-	fmt.Printf("Circumference %.2f\n", circumference)
+	fmt.Println("Date and Time")
 
 }
