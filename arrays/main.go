e096a480a145d6822e9b84f5751f98a87504592f
Timur Titov <t.titov@criteo.com>
2024-12-24 15:19:49 +0200

    added arrays

---
+++ arrays/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	var colors [3]string
+	colors[0] = "Red"
+	colors[1] = "Yellow"
+	colors[2] = "Green"
+
+	fmt.Println(colors)
+	fmt.Println(colors[0])
+	fmt.Println(colors[1])
+
+}
